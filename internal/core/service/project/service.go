dc71b3844314f72372f90a5c05e34e5ee46f496c
Robbyn Rahmandaru <robzlabz@gmail.com>
2024-05-07 08:31:38 +0700

    feat: implement interface

--- internal/core/service/project/service.go
+++ internal/core/service/project/service.go
@@ -10,3 +10,50 @@ type Service interface {
 	DonateProject() (err error)
 	ListDonation() (err error)
 }
+
+type service struct {
+}
+
+func NewService() Service {
+	return &service{}
+}
+
+func (s *service) RequestUploadURL() (err error) {
+	// todo: implement repository for get upload url
+	return
+}
+
+func (s *service) UplaodProjectImage() (err error) {
+	// todo : implement repository for upload image
+	return
+}
+
+func (s *service) SubmitProject() (err error) {
+	// todo: implement repository for submit project
+	return
+}
+
+func (s *service) ReviewProjectByAdmin() (err error) {
+	// todo: implement repository for review project
+	return
+}
+
+func (s *service) ListProject() (err error) {
+	// todo: implement repository for list project
+	return
+}
+
+func (s *service) GetProjectDetail() (err error) {
+	// todo: implement repository for get project detail
+	return
+}
+
+func (s *service) DonateProject() (err error) {
+	// todo: implement repository for donate project
+	return
+}
+
+func (s *service) ListDonation() (err error) {
+	// todo : implement repository for list donation
+	return
+}
--- internal/core/service/user/service.go
+++ internal/core/service/user/service.go
@@ -5,3 +5,25 @@ type Service interface {
 	LoginUser() (err error)
 	GetListUser() (err error)
 }
+
+type service struct {
+}
+
+func NewService() Service {
+	return &service{}
+}
+
+func (s *service) RegisterUser() (err error) {
+	// todo : implement repository for register user
+	return
+}
+
+func (s *service) LoginUser() (err error) {
+	// todo : implement repository for login user
+	return
+}
+
+func (s *service) GetListUser() (err error) {
+	// todo : implement repository for get list user
+	return
+}
